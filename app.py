2a6e7763c3c6286c49f28373fd88c9cbb6dabe70
Raghuvaran Eswaran <66511638+eraghu21@users.noreply.github.com>
2026-02-25 13:30:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,9 @@ def generate_certificate(student, score, cert_id):
      # ---------------- DEPT - YEAR - SEC ----------------
     pdf.set_font("Arial", "", 18)
     pdf.set_xy(0, 110)
-    pdf.cell(297, 10, f"{Section} -  {year} -  {Dept}", align="C")
+    pdf.cell(297, 10, 
+         f"{student['Section']} - {student['Year']} - {student['Dept']}", 
+         align="C")
 
     
     # ---------------- ROUND SCORE BADGE (RIGHT SIDE) ----------------
